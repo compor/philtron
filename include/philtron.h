488ec78637aa389c44b15934e3eb91ac26d8d09d
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-02-07 14:28:41 +0000

    declare alloc func protorypes

--- include/philtron.h
+++ include/philtron.h
@@ -2,12 +2,16 @@
 #ifndef PHILTRON_H
 #define PHILTRON_H
 
+#include <stdlib.h>
+
 
 /* external calls to be filtered */
 
 #define EXTERNAL_MALLOC jemalloc_malloc
 #define EXTERNAL_FREE jemalloc_free
 
+typedef void *(*external_malloc_t)(size_t);
+typedef void (*external_free_t)(void *);
 
 
 #endif /* PHILTRON_H */
