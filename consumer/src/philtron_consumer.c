5a252ed54c0d36c2ff0433e358dff62025d28186
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-02-08 09:09:42 +0000

    use a bit more complex allocation pattern

--- consumer/src/philtron_consumer.c
+++ consumer/src/philtron_consumer.c
@@ -5,13 +5,23 @@
 
 
 int main(int argc, char *argv[]) {
-  int *ptr = NULL;
+  int *ptr1 = NULL;
+  int *ptr2 = NULL;
   int i = 0;
 
   for(i = 0; i < 10; i++) {
-    ptr = jemalloc_malloc(sizeof(int));
-    jemalloc_free(ptr);
+    ptr1 = jemalloc_malloc(sizeof(int));
+    jemalloc_free(ptr1);
   }
+
+  ptr2 = jemalloc_malloc(100 * sizeof(int));
+
+  for(i = 0; i < 10; i++) {
+    ptr1 = jemalloc_malloc(sizeof(int));
+    jemalloc_free(ptr1);
+  }
+
+  jemalloc_free(ptr2);
 
   return 0;
 }
